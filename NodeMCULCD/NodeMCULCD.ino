1f683f94aa303b8b0d402133a47dabd73fbc4307
Globul <noemail@noemail.com>
2018-10-15 11:54:09 +0200

    Update NodeMCULCD

--- NodeMCULCD/NodeMCULCD.ino
+++ NodeMCULCD/NodeMCULCD.ino
@@ -7,8 +7,33 @@
 //LiquidCrystal_I2C lcd(0x3F, 16, 2);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+
+// DISPLAY I2C 16x2 characters
+// Display --- NodeMCU
+//   GND   ---   GND
+//   VCC   ---   Vin
+//   SDA   ---   D2
+//   SCL   ---   D1
+
+
+const int Btn1 = 14;
+const int Btn2 = 12;
+const int Btn3 = 13;
+const int Btn4 = 15;
+
 void setup() {
 
+  pinMode(Btn1, INPUT);
+  pinMode(Btn2, INPUT);
+  pinMode(Btn3, INPUT);
+  pinMode(Btn4, INPUT);  
+
+  // use mode HIGH for the buttons to limit the bounds
+/*  digitalWrite(Btn1, HIGH);
+  digitalWrite(Btn2, HIGH);
+  digitalWrite(Btn3, HIGH);
+  digitalWrite(Btn4, HIGH);*/
+  
   // The begin call takes the width and height. This
   // Should match the number provided to the constructor.
   lcd.begin(16,2);
@@ -27,10 +52,27 @@ void setup() {
   // Move the cursor to the next line and print
   // WORLD.
   lcd.setCursor(5, 1);      
-  lcd.print("WORLD");
+//  lcd.print("WORLD");
 }
 
 void loop() {
+  lcd.setCursor(1, 1);
+  if (digitalRead(Btn1) == HIGH)
+  {
+    lcd.print("HIGH");
+  }
+  else
+  {
+    lcd.print("LOW ");
+  }
+
+  lcd.setCursor(10, 1);
+  if (digitalRead(Btn2) == HIGH)
+    lcd.print("HIGH");
+  else
+    lcd.print("LOW ");
+
+  delay(100);
 #if 0
 // Turn on the backlight.
   lcd.backlight();
