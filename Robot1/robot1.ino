ae84e2e9a88ec24a5d10db2cb0812581292d93c2
Globul <noemail@noemail.com>
2018-10-29 08:35:20 +0100

    Robot1: wip

--- Robot1/robot1.ino
+++ Robot1/robot1.ino
@@ -20,6 +20,8 @@ int ENR = 15;
 int IN1R = 1;//13
 int IN2R = 3;//12
 
+#define IDA  2
+
 // Max value for motor
 #define MAXSPEED  1024
 
@@ -242,6 +244,7 @@ void setup() {
 #define	ST_START		1		// auto, start phase
 #define	ST_FORWARD	2		// auto, forward phase
 #define	ST_TURN			3		// auto, turn phase
+#define ST_SCAN     4   // scan
 
 #define EVT_NONE				0		// no event
 #define EVT_MODESWITCH	1		// switch mode manual/auto
@@ -250,14 +253,37 @@ void setup() {
 #define EVT_SPEED0			4		// set speed to 0
 #define EVT_SELMOTORL		5		// select left motor
 #define EVT_SELMOTORR		6		// select right motor
+#define EVT_SELMOTORB   7   // select both motors
 
 int First=1;
-int	State = ST_MANUAL;
+int  State = ST_MANUAL;
+int  PrevState = ST_MANUAL;
 #define DTSZ  5
 int DT[DTSZ];
 int DTidx;
 int ModeAuto = 0;
 int	LastScan = -1;
+int Count = 0;
+
+void changeState(int st)
+{
+  PrevState = State;
+  State = st;
+}
+
+void dispMsg(char *str)
+{
+  static int count = 0;
+  
+  if (count % 4 == 0)
+  {
+    display.clearDisplay();
+    display.setCursor(0,0);
+  }
+  display.println(str);
+  display.display();
+  count++;
+}
 
 int	manualMode(int evt)
 {
@@ -275,48 +301,99 @@ int	manualMode(int evt)
 			break;
 
 		case	EVT_SPEEDINC:
-			if (motorSel == IDL)
+      dispMsg("inc speed");
+			if (motorSel == IDL || motorSel == IDA)
 			{
 				speedL += MAXSPEED/10;
 				speed = speedL;
 			}
-			else
+      if (motorSel == IDR || motorSel == IDA)
 			{
 				speedR += MAXSPEED/10;
 				speed = speedR;
 			}
-			setMotor(motorSel, speed);
+      if (motorSel == IDA)
+      {
+        setMotor(IDL, speed);
+        setMotor(IDR, speed);
+      }
+      else
+        setMotor(motorSel, speed);
 			break;
 
 		case	EVT_SPEEDDEC:
-			if (motorSel == IDL)
+      dispMsg("dec speed");
+      if (motorSel == IDL || motorSel == IDA)
 			{
 				speedL -= MAXSPEED/10;
 				speed = speedL;
 			}
-			else
+      if (motorSel == IDR || motorSel == IDA)
 			{
 				speedR -= MAXSPEED/10;
 				speed = speedR;
 			}
 			setMotor(motorSel, speed);
 			break;
-	}
+    case  EVT_SELMOTORB:
+      dispMsg("select motor both");
+      motorSel = IDA;
+      break;
+    case  EVT_SELMOTORR:    	
+      dispMsg("select motor right");
+      motorSel = IDR;
+      break;
+    case  EVT_SELMOTORL:
+      dispMsg("select motor left");
+      motorSel = IDL;
+      break;
+  }
 }
 
 int start(int evt)
 {
+  int i;
+  
+  dispMsg("State = ST_START");
+  for(i=0; i<DTSZ; i++)
+    DT[i] = -1;
+  DTidx=0;
+  changeState(ST_FORWARD);
+  return 0;
 }
 
 int forward(int evt)
 {
-	static	int	speed=0;
+	static	int	speed=0.8*MAXSPEED;
+  static  int lightTurn=0;  // light turn when approaching obstacle
 	int	ave=0, i;
 	char	buf[40];
+
+//  dispMsg("State = ST_FORWARD");
 
 	if (LastScan < 0)
 		return -1;
 
+  if (LastScan == 0)
+  {
+    speed = -20000;
+    setMotor(IDL, speed);
+    setMotor(IDR, speed);
+    dispMsg("FREINAGE!");
+    delay(100);
+    dispMsg("Arriere");
+    speed = -0.4*MAXSPEED;
+    setMotor(IDL, speed);
+    setMotor(IDR, speed);
+    delay(500);
+    dispMsg("Stop");
+    speed = 0;
+    setMotor(IDL, speed);
+    setMotor(IDR, speed);
+    changeState(ST_TURN);
+    return 1;
+  }
+  
 	DT[DTidx++] = LastScan;
 	if (DTidx >= DTSZ)
 		DTidx = 0;
@@ -328,6 +405,8 @@ int forward(int evt)
 		{
 			sprintf(buf, "DT[%d}<0", i);
 			//      Serial.println(buf);
+      setMotor(IDL, 0);
+      setMotor(IDR, 0);
 			return 0;
 		}
 		ave += DT[i];
@@ -335,27 +414,62 @@ int forward(int evt)
 
 	ave = ave / DTSZ;
 
-	if (ave > 100)
+	if (ave > 70)
 	{
+    if (speed < MAXSPEED)
+      speed += MAXSPEED*0.1;
+    else
+      speed = MAXSPEED;
 		setMotor(IDL, speed);
 		setMotor(IDR, speed);
+    // just select lightTurn way for next time
+    if (lightTurn == 0)
+      lightTurn = random(0, 2)*2 - 1;
 	}
 	else if (ave > 20)
 	{
-		setMotor(IDL, ave*speed/100);
-		setMotor(IDR, ave*speed/100);
+    if (speed > ave*MAXSPEED/100)
+      speed = ave*MAXSPEED/100;
+    if (speed < MAXSPEED*0.3)
+      speed = MAXSPEED*0.3;
+    // obstacle detected, lightTurn to try to avoid
+		setMotor(IDL, speed+MAXSPEED*0.1*lightTurn);
+		setMotor(IDR, speed-MAXSPEED*0.1*lightTurn);
 	}
 	else
-	{
-		setMotor(IDL, -0.5*speed);
-		setMotor(IDR, 0.5*speed);
-		delay(200);
-	}
+  {
+    lightTurn = 0;
+    changeState(ST_TURN);
+  }
 }
 
 int turn(int evt)
 {
+  static int fact=0;
+
+  dispMsg("State = ST_TURN");
+
+  // don't change the way if scanning process running
+  if ((fact == 0 || PrevState != ST_SCAN) && random(0,2))
+    fact = -1;
+  else
+    fact = 1;
+
+  setMotor(IDL, -fact*0.4*MAXSPEED);
+  setMotor(IDR, fact*0.4*MAXSPEED);
+  delay(300);
+  changeState(ST_SCAN);
 	return 0;
+}
+
+int scan(int evt)
+{
+  dispMsg("State = ST_SCAN");
+  if (LastScan < 20)
+    changeState(ST_TURN);
+  else
+    changeState(ST_FORWARD);
+  return 0;
 }
 
 /*!
@@ -372,13 +486,13 @@ void  treatEvt(int evt)
 	{
 		if (State != ST_MANUAL)
 		{
-			State = ST_MANUAL;
+			changeState(ST_MANUAL);
 			manualMode(EVT_SPEED0);
 			return;
 		}
 		else
 		{
-			State = ST_START;
+			changeState(ST_START);
 		}
 	}
 
@@ -394,8 +508,11 @@ void  treatEvt(int evt)
 			forward(evt);
 			return;
 		case ST_TURN:
-			start(evt);
+			turn(evt);
 			return;
+    case ST_SCAN:
+      scan(evt);
+      return;
 		default:
 			break;
 	}
@@ -423,6 +540,7 @@ int readIR()
 		{
 			case T_1:     ret = EVT_SELMOTORL; break;
 			case T_2:     ret = EVT_SELMOTORR; break;
+      case T_3:     ret = EVT_SELMOTORB; break;
 			case T_VOLM:  ret = EVT_SPEEDDEC; break;
 			case T_VOLP:  ret = EVT_SPEEDINC; break;
 //			case T_PREV:  Speed=-MAXSPEED; break;
@@ -442,7 +560,6 @@ int readIR()
  */
 int sonarScan()
 {
-	static int	count=0;
 	int	duration, distance;
 	char	buf[40];
 
@@ -461,15 +578,11 @@ int sonarScan()
 	// Calculating the distance in cm
 	distance= duration*0.034/2;
 
-	if (count%4 == 0)
-	{
-		display.clearDisplay();
-		display.setCursor(0,0);
-	}
-	sprintf(buf, "distance=%d", distance);
-	display.println(buf);
-	display.display();
-	count++;
+  if (State != ST_MANUAL)
+  {
+   	sprintf(buf, "distance=%d", distance);
+	  dispMsg(buf);
+  }
 
 	return distance;
 }
@@ -483,18 +596,13 @@ void loop()
 	char  tmp[128];
 	int   key, i, evt;
 
-	if (First)
-	{
-		//    Serial.println("IR remote codes");
-		First = 0;
-		for(i=0; i<DTSZ; i++)
-			DT[i] = -1;
-		DTidx=0;
-	}
-
-	LastScan = sonarScan();
-
 	evt = readIR();
+  if (evt != EVT_NONE)
+  {
+    sprintf(buf, "evt = %d", evt);
+    dispMsg(buf);
+  }
+  LastScan = sonarScan();
 	treatEvt(evt);
 
 	return;
